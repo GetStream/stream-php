8b0ac8b1b9cae5665144e9c507e2333d51cd1960
Tommaso Barbugli <tbarbugli@gmail.com>
2014-05-13 14:28:54 +0200

    initial structure

---
+++ lib/GetStream/Stream/Client.php
@@ -0,0 +1,15 @@
+<?php
+namespace GetStream\Stream;
+
+/**
+ * A terribly useful class
+ *
+ * See: http://knpuniversity.com/screencast/question-answer-day
+ */
+class Client
+{
+    public function sayHallo()
+    {
+        return 'Hallo!';
+    }
+}
