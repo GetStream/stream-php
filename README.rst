c112d7b3bf2dd0469fd8aef797d928d79e0fbd96
Tommaso Barbugli <tbarbugli@gmail.com>
2014-05-19 16:56:34 +0200

    add readme

---
+++ README.rst
@@ -0,0 +1,86 @@
+stream-php
+=========
+
+.. image:: https://secure.travis-ci.org/tbarbugli/stream-php.png?branch=master
+   :target: http://travis-ci.org/tbarbugli/stream-php
+
+
+stream-php is a PHP client for `Stream <https://getstream.io/>`_.
+
+.. code-block:: php
+
+    // Instantiate a new client
+    $client = new new GetStream\Stream\Client('YOUR_API_KEY', 'API_KEY_SECRET');
+
+    // Instantiate a feed object
+    $user_feed_1 = $client->feed('user:1');
+
+    // Get activities from 5 to 10
+    $results = $user_feed_1->getActivities(5, 10);
+
+    // Create a new activity
+    $data = [
+        "actor_id"=>"1",
+        "verb"=>"like",
+        "object_id"=>"3"
+    ];
+    $user_feed_1->addActivity($data);
+
+    // Remove an activity by its id
+    $user1->removeActivity('12345678910');
+
+    // Follow another feed
+    $user1->followFeed('flat:42');
+
+    // Stop following another feed
+    $user1->unfollowFeed('flat:42');
+
+
+Installation
+------------
+
+Install with Composer
+~~~~~~~~~~~~~~~~~~~~~
+
+If you're using `Composer <https://getcomposer.org/>`_ to manage
+dependencies, you can add Stream with it.
+
+.. code-block:: json
+
+    {
+        "require": {
+            "get-stream/stream": "$VERSION"
+        }
+    }
+
+(replace ``$VERSION`` with one of the available versions on `Packagist <https://packagist.org/packages/raven/raven>`_)
+or to get the latest version off the master branch:
+
+.. code-block:: json
+
+    {
+        "require": {
+            "get-stream/stream": "dev-master"
+        }
+    }
+
+Note that using unstable versions is not recommended and should be avoided.
+
+Composer will take care of the autoloading for you, so if you require the
+``vendor/autoload.php``, you're good to go.
+
+
+Contributing
+------------
+
+First, make sure you can run the test suite. Install development dependencies :
+
+::
+
+    $ composer install
+    
+You may now use phpunit :
+
+::
+
+    $ vendor/bin/phpunit
--- test/FeedTest.php
+++ test/FeedTest.php
@@ -40,9 +40,9 @@ class FeedTest extends \PHPUnit_Framework_TestCase
         $feed = new TestFeed('feed:1', 'api', 'token');
         $data = ["name" => "php client"];
         $feed->addActivity($data);
-        $lastRequest = TestFeed::getHistory()->getLastRequest();
-        $this->assertSame($lastRequest->getUrl(), "https://getstream.io/api/feed/feed/1/?api_key=api");
-        $this->assertSame($lastRequest->getMethod(), "POST");
+        $lastReq = TestFeed::getHistory()->getLastRequest();
+        $this->assertSame($lastReq->getUrl(), "https://getstream.io/api/feed/feed/1/?api_key=api");
+        $this->assertSame($lastReq->getMethod(), "POST");
     }
 
     public function testClientFeedGetActivities()
@@ -54,16 +54,16 @@ class FeedTest extends \PHPUnit_Framework_TestCase
         $offset = 3;
 
         $response = $feed->getActivities($offset, $limit);
-        $lastRequest = TestFeed::getHistory()->getLastRequest();
-        $this->assertSame($lastRequest->getUrl(), "https://getstream.io/api/feed/feed/1/?offset=3&limit=1&api_key=api");
+        $lastReq = TestFeed::getHistory()->getLastRequest();
+        $this->assertSame($lastReq->getUrl(), "https://getstream.io/api/feed/feed/1/?offset=3&limit=1&api_key=api");
 
         $response = $feed->getActivities($offset);
-        $lastRequest = TestFeed::getHistory()->getLastRequest();
-        $this->assertSame($lastRequest->getUrl(), "https://getstream.io/api/feed/feed/1/?offset=3&limit=20&api_key=api");
+        $lastReq = TestFeed::getHistory()->getLastRequest();
+        $this->assertSame($lastReq->getUrl(), "https://getstream.io/api/feed/feed/1/?offset=3&limit=20&api_key=api");
 
         $response = $feed->getActivities();
-        $lastRequest = TestFeed::getHistory()->getLastRequest();
-        $this->assertSame($lastRequest->getUrl(), "https://getstream.io/api/feed/feed/1/?offset=0&limit=20&api_key=api");
+        $lastReq = TestFeed::getHistory()->getLastRequest();
+        $this->assertSame($lastReq->getUrl(), "https://getstream.io/api/feed/feed/1/?offset=0&limit=20&api_key=api");
     }
 
     public function testClientremoveActivity()
@@ -71,9 +71,9 @@ class FeedTest extends \PHPUnit_Framework_TestCase
         $feed = new TestFeed('feed:1', 'api', 'token');
         $aid = '123';
         $response = $feed->removeActivity($aid);
-        $lastRequest = TestFeed::getHistory()->getLastRequest();
-        $this->assertSame($lastRequest->getUrl(), "https://getstream.io/api/feed/feed/1/123/?api_key=api");
-        $this->assertSame($lastRequest->getMethod(), "DELETE");
+        $lastReq = TestFeed::getHistory()->getLastRequest();
+        $this->assertSame($lastReq->getUrl(), "https://getstream.io/api/feed/feed/1/123/?api_key=api");
+        $this->assertSame($lastReq->getMethod(), "DELETE");
     }
 
     public function testClientFollow()
@@ -81,9 +81,9 @@ class FeedTest extends \PHPUnit_Framework_TestCase
         $feed = new TestFeed('feed:1', 'api', 'token');
         $target = 'feed:123';
         $response = $feed->followFeed($target);
-        $lastRequest = TestFeed::getHistory()->getLastRequest();
-        $this->assertSame($lastRequest->getUrl(), "https://getstream.io/api/feed/feed/1/follows/?api_key=api");
-        $this->assertSame($lastRequest->getMethod(), "POST");
+        $lastReq = TestFeed::getHistory()->getLastRequest();
+        $this->assertSame($lastReq->getUrl(), "https://getstream.io/api/feed/feed/1/follows/?api_key=api");
+        $this->assertSame($lastReq->getMethod(), "POST");
     }
 
     public function testClientUnfollow()
@@ -91,8 +91,8 @@ class FeedTest extends \PHPUnit_Framework_TestCase
         $feed = new TestFeed('feed:1', 'api', 'token');
         $target = 'feed:123';
         $response = $feed->unfollowFeed($target);
-        $lastRequest = TestFeed::getHistory()->getLastRequest();
-        $this->assertSame($lastRequest->getUrl(), "https://getstream.io/api/feed/feed/1/follows/feed:123/?api_key=api");
-        $this->assertSame($lastRequest->getMethod(), "DELETE");
+        $lastReq = TestFeed::getHistory()->getLastRequest();
+        $this->assertSame($lastReq->getUrl(), "https://getstream.io/api/feed/feed/1/follows/feed:123/?api_key=api");
+        $this->assertSame($lastReq->getMethod(), "DELETE");
     }
 }
