785b1f94917cc2edb9e737d2a578004ac92addf5
Peter van Kampen <pterk@datatailors.com>
2019-02-04 18:17:45 +0100

    update collections API

--- lib/GetStream/Stream/Collections.php
+++ lib/GetStream/Stream/Collections.php
@@ -50,6 +50,10 @@ class Collections
         if( $method === 'POST' || $method === 'PUT' ){
             $params = ['json' => $params];
         }
+        if( $method === 'GET' && $params !== null ){
+
+            $endpoint .= '?' . http_build_query($params);
+        }
         try {
             $response = $this->client->request($method, $endpoint, $params);
         } catch (ClientException $e) {
@@ -64,6 +68,22 @@ class Collections
 
     /**
      * @param string $collectionName
+     * @param array $data
+     * @param string $id (optional)
+     * @param string $user_id (optional)
+     *
+     * @return array
+     */
+    public function add($collectionName, array $data, $id=null, $user_id=null)
+    {
+        $payload = ["id" => $id, "data" => $data, "user_id" => $user_id];
+        $response = $this->doRequest('POST', 'collections/' . $collectionName . '/', $payload);
+        $body = $response->getBody()->getContents();
+        return json_decode($body, true);
+    }
+
+    /**
+     * @param string $collectionName
      * @param string $id
      *
      * @return string
@@ -71,6 +91,48 @@ class Collections
     public function createReference($collectionName, $id)
     {
         return "SO:".$collectionName.":".$id;
+    }
+
+    /**
+     * @param string $collectionName
+     * @param string $id
+     *
+     * @return array
+     */
+    public function delete($collectionName, $id)
+    {
+
+        $response = $this->doRequest('DELETE', 'collections/' . $collectionName . '/' . $id . '/');
+        $body = $response->getBody()->getContents();
+        return json_decode($body, true);
+    }
+
+    /**
+     * @param string $collectionName
+     * @param array $ids
+     *
+     * @return array
+     */
+    public function deleteMany($collectionName, array $ids)
+     {
+        $ids = join(',', $ids);
+        $queryParams = ['collection_name' => $collectionName, 'ids' => $ids];
+        $response = $this->client->request('DELETE', 'collections/?'.http_build_query($queryParams));
+        $body = $response->getBody()->getContents();
+        return json_decode($body, true);
+     }
+
+    /**
+     * @param string $collectionName
+     * @param string $id
+     *
+     * @return array
+     */
+    public function get($collectionName, $id)
+    {
+        $response = $this->doRequest('GET', 'collections/' . $collectionName . '/' . $id . '/');
+        $body = $response->getBody()->getContents();
+        return json_decode($body, true);
     }
 
     /**
@@ -92,6 +154,21 @@ class Collections
 
     /**
      * @param string $collectionName
+     * @param string $id
+     * @param array $data
+     *
+     * @return array
+     */
+    public function update($collectionName, $id, array $data)
+    {
+        $payload = ["data" => $data];
+        $response = $this->doRequest('PUT', 'collections/' . $collectionName . '/' . $id . '/', $payload);
+        $body = $response->getBody()->getContents();
+        return json_decode($body, true);
+    }
+
+    /**
+     * @param string $collectionName
      * @param array $data
      *
      * @return array
@@ -100,32 +177,6 @@ class Collections
     {
         $response = $this->doRequest('POST', 'meta/', ['data' => [$collectionName => array($data)]]);
         $body = $response->getBody()->getContents();
-        return json_decode($body, true);
-    }
-
-    /**
-     * @param string $collectionName
-     * @param array $ids
-     *
-     * @return array
-     */
-    public function delete($collectionName, array $ids)
-    {
-        $ids = join(',', $ids);
-        $queryParams = ['collection_name' => $collectionName, 'ids' => $ids];
-
-        try {
-            $response = $this->client->request('DELETE', 'meta/?'.http_build_query($queryParams));
-        } catch (ClientException $e) {
-            $response = $e->getResponse();
-            $msg = $response->getBody();
-            $code = $response->getStatusCode();
-            $previous = $e;
-            throw new StreamFeedException($msg, $code, $previous);
-        }
-
-        $body = $response->getBody()->getContents();
-
         return json_decode($body, true);
     }
 
