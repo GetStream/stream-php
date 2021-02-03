3c1715a724d6d0fd96652d6797cf3df5d5ae9c96
ferhat elmas <elmas.ferhat@gmail.com>
2021-02-03 19:11:14 +0100

    Extract handler to util

---
+++ lib/GetStream/Stream/Util.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace GetStream\Stream;
+
+use Firebase\JWT\JWT;
+use GuzzleHttp\HandlerStack;
+use Psr\Http\Message\RequestInterface;
+
+class Util {
+    /**
+     * @return HandlerStack
+     */
+    public static function handlerStack($apiKey, $apiSecret, $resource)
+    {
+        $token = JWT::encode([
+            'action' => '*',
+            'user_id' => '*',
+            'feed_id' => '*',
+            'resource' => $resource,
+        ], $apiSecret, 'HS256');
+        $stack = HandlerStack::create();
+        $stack->push(function (callable $handler) use ($token, $apiKey) {
+            return function (RequestInterface $request, array $options) use ($handler, $token, $apiKey) {
+                // Add authentication headers.
+                $request = $request
+                    ->withAddedHeader('Authorization', $token)
+                    ->withAddedHeader('Stream-Auth-Type', 'jwt')
+                    ->withAddedHeader('Content-Type', 'application/json')
+                    ->withAddedHeader('X-Stream-Client', 'stream-php-client-' . VERSION);
+                // Add a api_key query param.
+                $queryParams = \GuzzleHttp\Psr7\parse_query($request->getUri()->getQuery());
+                $query = http_build_query($queryParams + ['api_key' => $apiKey]);
+                $request = $request->withUri($request->getUri()->withQuery($query));
+                return $handler($request, $options);
+            };
+        });
+        return $stack;
+    }
+}
